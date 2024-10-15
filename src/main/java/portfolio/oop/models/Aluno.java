9eb94f4af084a4a2347b01d8bc4c1dfd811107e2
leonardoGasperin <leonardovdg@hotmail.com>
2024-10-15 14:48:27 -0300

    implementa aluno

--- src/main/java/portfolio/oop/models/Aluno.java
+++ src/main/java/portfolio/oop/models/Aluno.java
@@ -1,5 +1,28 @@
 package portfolio.oop.models;
 
-public class Aluno {
+public class Aluno extends Pessoa {
+    public String matricula;
+    public String curso;
     
+    public Aluno (String nome, int idade, String matricula, String curso){
+        super(nome, idade);
+        SetMatricula(matricula);
+        SetCurso(curso);
+    }
+
+    private void SetMatricula(String matricula){
+        this.matricula = matricula;
+    }
+
+    private void SetCurso(String matricula){
+        this.matricula = matricula;
+    }
+
+    public String GetMatricula(String matricula){
+        return this.matricula;
+    }
+
+    public String GetCurso(String matricula){
+        return this.matricula;
+    }
 }
