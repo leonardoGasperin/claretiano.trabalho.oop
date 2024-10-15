a8203f343413cea99fafa5ca6a711469323fcbe5
leonardoGasperin <leonardovdg@hotmail.com>
2024-10-15 18:52:03 -0300

    ImprimePessoaInfo

--- src/main/java/portfolio/oop/models/Pessoa.java
+++ src/main/java/portfolio/oop/models/Pessoa.java
@@ -33,8 +33,8 @@ public class Pessoa {
         this.SetIdade(idade);
     }
 
-    public void ImprimePessoaInfo(Pessoa pessoa){
-        System.out.println( "Nome: " + this.GetNome() +
-                            "\nIdade: " + this.GetIdade());
+    public String ImprimePessoaInfo(Pessoa pessoa){
+        return "Nome: " + this.GetNome() +
+               "\nIdade: " + this.GetIdade();
     }
 }
