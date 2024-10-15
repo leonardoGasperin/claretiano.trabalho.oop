bf8e1c8af80182e9700fa89875dad528033380f9
leonardoGasperin <leonardovdg@hotmail.com>
2024-10-15 13:11:17 -0300

    first commit

---
+++ src/test/java/portfolio/oop/AppTest.java
@@ -0,0 +1,14 @@
+package portfolio.oop;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
