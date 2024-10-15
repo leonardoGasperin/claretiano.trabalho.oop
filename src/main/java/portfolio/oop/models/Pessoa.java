cd3d7e8fe2c19436ab16cf0327456a7a8442a54c
leonardoGasperin <leonardovdg@hotmail.com>
2024-10-15 14:40:23 -0300

    implementa modelo Pessoa

--- src/main/java/portfolio/oop/models/Pessoa.java
+++ src/main/java/portfolio/oop/models/Pessoa.java
@@ -1,5 +1,27 @@
 package portfolio.oop.models;
 
 public class Pessoa {
+    private String nome;
+    private int idade;
     
+    public Pessoa(String nome, int idade){
+        this.SetName(nome);
+        this.SetIdade(idade);
+    }
+
+    public String GetName(String nome){
+        return this.nome;
+    }
+
+    public int GetIdade(int idade){
+        return this,idade;
+    }
+
+    private void SetName(String nome){
+        this.nome = nome;
+    }
+
+    private void SetIdade(int idade){
+        this.idade = idade;
+    }
 }
