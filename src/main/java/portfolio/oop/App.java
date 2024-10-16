3b8b342d60fad73ea81f7830af0c0cbf6b0c1766
leonardoGasperin <leonardovdg@hotmail.com>
2024-10-15 21:12:56 -0300

    implementação da requisição de codigo em main

--- src/main/java/portfolio/oop/App.java
+++ src/main/java/portfolio/oop/App.java
@@ -1,9 +1,27 @@
 package portfolio.oop;
 
+import portfolio.oop.models.Aluno;
+import portfolio.oop.models.Pessoa;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Pessoa pessoa = new Pessoa("José Pessoa", 20);
+        Aluno aluno = new Aluno(pessoa.GetNome(), pessoa.GetIdade(), "CBXMQ004", "Programação Orientada a Objetos");
+        Aluno aluno2 = new Aluno("Leonardo Vinicius De Gasperin", 32, "Sei lá", "OOP");
+        
+        System.out.println(pessoa.GetPessoaData("Informações de pessoa Original"));
+        System.out.println(aluno.GetAlunoData("Informações de aluno Original"));
+
+        aluno.EditNome(null);
+        aluno.EditIdade(0);
+        aluno.EditMatricula(null);
+        aluno.EditCurso(null);
+
+        System.out.println(aluno.GetAlunoData("Informações de aluno editado"));
+        System.out.println(pessoa.GetPessoaData("Informações de pessoa após aluno ser editado"));
+        System.out.println(aluno.GetPessoaData("Informações da pessoa de aluno após edição"));
+        System.out.println(aluno2.GetPessoaData("EU"));
     }
 }
